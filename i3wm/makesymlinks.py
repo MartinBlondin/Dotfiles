a1702325dd52e71cf7dfdec8fec71a27fb38329b
Martin Blondin <xxhodeb@gmail.com>
2017-09-10 00:01:08 +0200

    made script to setup i3

---
+++ i3wm/makesymlinks.py
@@ -0,0 +1,17 @@
+import subprocess
+from pathlib import Path
+
+home = Path.home
+home = str(Path.home()) + '/'
+
+
+def link(file):
+    subprocess.call(['ln', '-s', home + 'dotfiles/i3wm/' + file, home + file])
+
+
+link('bgscript.py')
+link('starti3.py')
+link('makesexy.py')
+
+subprocess.call(['mkdir', home + 'bg'])
+subprocess.call(['cp', home + 'dotfiles/bg/*', home + 'bg/'])
