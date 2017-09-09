dc087bb774a935d43af8ff752cb1f8c8d597f03b
Martin Blondin <xxhodeb@gmail.com>
2017-09-09 23:39:50 +0200

    Made i3 automatically call the start script

---
+++ i3wm/starti3.py
@@ -0,0 +1,99 @@
+import pyautogui as pag
+import time
+
+
+def openShell():
+    pag.keyDown('winleft')
+    pag.press('enter')
+    pag.keyUp('winleft')
+    return()
+
+
+def setVertical():
+    pag.keyDown('winleft')
+    pag.press('v')
+    pag.keyUp('winleft')
+    return()
+
+
+def setHorizontal():
+    pag.keyDown('winleft')
+    pag.press('g')
+    pag.keyUp('winleft')
+    return()
+
+
+def openEmacs():
+    pag.keyDown('winleft')
+    pag.press('z')
+    pag.keyUp('winleft')
+    return()
+
+
+def openFirefox():
+    pag.keyDown('winleft')
+    pag.press('p')
+    pag.keyUp('winleft')
+    return()
+
+
+def openSyncthing():
+    pag.keyDown('winleft')
+    pag.press('enter')
+    pag.keyUp('winleft')
+    pag.typewrite('syncthing -no-browser')
+    pag.press('enter')
+    return()
+
+
+def openMakesexy():
+    pag.keyDown('winleft')
+    pag.press('enter')
+    pag.keyUp('winleft')
+    pag.typewrite('python makesexy.py')
+    pag.press('enter')
+    return()
+
+
+def gotoWorkspace(workspaceId):
+    pag.keyDown('winleft')
+    pag.press(str(workspaceId))
+    pag.keyUp('winleft')
+    return()
+
+
+def sendtoWorkspace(workspaceId):
+    pag.keyDown('winleft')
+    pag.keyDown('shift')
+    pag.press(str(workspaceId))
+    pag.keyUp('shift')
+    pag.keyUp('winleft')
+    return()
+
+
+gotoWorkspace(1)
+setVertical()
+openShell()
+setHorizontal()
+openShell()
+setVertical()
+openShell()
+
+gotoWorkspace(4)
+openSyncthing()
+setVertical()
+openMakesexy()
+
+gotoWorkspace(2)
+openFirefox()
+
+gotoWorkspace(3)
+openEmacs()
+
+time.sleep(2)
+
+gotoWorkspace(3)
+sendtoWorkspace(9)
+gotoWorkspace(9)
+sendtoWorkspace(3)
+gotoWorkspace(2)
