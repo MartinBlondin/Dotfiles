a669e4449a1f16876a9a4c488b73e01f9c474bdd
Martin Blondin <xxhodeb@gmail.com>
2018-03-06 20:11:39 +0100

    stuff and things

--- scripts/hide-mouse.py
+++ scripts/hide-mouse.py
@@ -8,8 +8,6 @@ width  = pag.size()[0] - 1
 height = pag.size()[1]
 
 if pag.position()[0] == width:
-    pag.moveTo(width / 2, (height / 2) - 1)
-    sleep(0.001)
-    pag.moveRel((0, 1))
+    pag.moveTo(width / 2, (height / 2) - 1); sleep(0.001); pag.moveRel((0, 1))
 else:
     pag.moveTo(width, height)
