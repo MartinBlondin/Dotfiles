762908332e7fff7b433fe1079875f0db084a7534
zoeybee <lurunder@gmail.com>
2021-05-26 14:29:35 +0200

    moved local commands

--- scripts/local_commands_after_startup.py
+++
@@ -1,9 +0,0 @@
-from i3_pyautogui_bindings import  open_shell, close, set_wait_time
-import pyautogui as pag
-from time import sleep
-
-'''
-Changes to this file should not be commited
-'''
-
-pass
--- scripts/local_commands_before_startup.py
+++
@@ -1,8 +0,0 @@
-import pyautogui as pag
-from i3_pyautogui_bindings import  open_shell, set_wait_time, close
-
-'''
-Changes to this file should not be commited
-'''
-
-pass
